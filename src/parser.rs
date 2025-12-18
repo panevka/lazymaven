d2d3bdcb27fc066e14c08e8b1b0c4eb7cfff9ab7
panevka <97028404+panevka@users.noreply.github.com>
2025-12-18 19:21:57 +0100

    feat: add parsing of a dependency tree

---
+++ src/parser.rs
@@ -0,0 +1,51 @@
+use std::{collections::HashMap, fs, io, iter::Map};
+
+use xmltree::Element;
+
+pub struct MavenFile {
+    root: Element,
+}
+
+impl MavenFile {
+    pub fn from_file(file_path: String) -> Result<Self, std::io::Error> {
+        let file_content: String = fs::read_to_string(file_path)?;
+        let xml_tree_root = Element::parse(file_content.as_bytes())
+            .map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?;
+        return Ok(Self {
+            root: xml_tree_root,
+        });
+    }
+
+    pub fn get_dependencies(&self) -> Vec<HashMap<String, String>> {
+        let mut dependencies: Vec<HashMap<String, String>> = Vec::new();
+
+        let deps_root = match self.root.get_child("dependencies") {
+            Some(d) => d,
+            None => return dependencies,
+        };
+
+        for dependency in &deps_root.children {
+            let mut dependency_map: HashMap<String, String> = HashMap::new();
+
+            if let Some(dependency_fields) = dependency.as_element() {
+                for field in &dependency_fields.children {
+                    let field_element = field.as_element();
+
+                    if let Some(element) = field_element {
+                        let field_name = &element.name;
+                        let field_value = element.children[0].as_text();
+
+                        dependency_map.insert(
+                            field_name.clone(),
+                            field_value.unwrap_or("NULL").to_string(),
+                        );
+                    }
+                }
+            }
+
+            dependencies.push(dependency_map);
+        }
+
+        return dependencies;
+    }
+}
